6813006c2e84c29ad50e3120089cad5e1ad215e3
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-04-27 13:45:55 -0700

    fix starter code bugs

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Start up DB Server
+require('dotenv').config()
+const mongoose = require('mongoose');
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+mongoose.connect(process.env.MONGODB_URI, options);
+
+// Start the web server
+require('./src/server.js').start(process.env.PORT);
